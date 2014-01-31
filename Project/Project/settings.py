0d20bfeae1d4fe1c6220ae328d5684fa6e68464b
Christopher Primerano <cprimera@sympatico.ca>
2014-01-31 21:57:33 +0000

    Updated basic skeleton

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'rest_framework',
+	'skvallra',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,3 +85,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+	    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+	    'PAGINATE_BY': 10
+}
---
+++ Project/skvallra/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from skvallra import views
+
+urlpatterns = patterns('',
+    
+)
+
